7cdd6f6beefb2a009cfe4736591b6aead5561d9d
aisahabalive-ux <aisahabalive@gmail.com>
2026-01-17 15:50:49 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,10 +4,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    // Ye batata hai ki files bina extension ke bhi mil jayein
     extensions: ['.js', '.ts', '.jsx', '.tsx']
-  },
-  build: {
-    outDir: 'dist'
   }
 })
