c9c11384ce885b84b883a78e4dda673c6e426961
aisahabalive-ux <aisahabalive@gmail.com>
2026-01-17 13:14:45 +0530

    Create types.ts

---
+++ types.ts
@@ -0,0 +1,14 @@
+// Is file ko GitHub par 'types.ts' naam se save karein
+export interface HybridSystemData {
+  status: string;
+  engine: string;
+  version: string;
+  lastSync: string;
+}
+
+export interface NexusState {
+  isActive: boolean;
+  loadFactor: number;
+  efficiency: string;
+}
+
