67452e1387f3f73a3b359650d539705cccf2f8b1
aisahabalive-ux <aisahabalive@gmail.com>
2026-02-15 20:44:09 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -4,6 +4,7 @@ import App from './App';
 import './index.css';
 
 const rootElement = document.getElementById('root');
+
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
   root.render(
