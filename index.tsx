c7942cc6997d98cdad96056e23679411532cad3d
aisahabalive-ux <aisahabalive@gmail.com>
2026-02-15 20:49:32 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-const rootElement = document.getElementById('root');
+// Yahan 'as HTMLElement' add karne se error chala jayega
+const rootElement = document.getElementById('root') as HTMLElement;
 
 if (rootElement) {
   const root = ReactDOM.createRoot(rootElement);
