3a5bbb53bbc2d5295a75ffcb140a5a992e58a66c
aisahabalive-ux <aisahabalive@gmail.com>
2026-02-15 15:18:56 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/supreme-v1000-hybrid/',
   build: {
     outDir: 'dist',
-  }
-})
+    emptyOutDir: true,
+  },
+  publicDir: 'public' 
+});
