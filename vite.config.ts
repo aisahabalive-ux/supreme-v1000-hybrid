246f5a101de1e4744b53712b1fa1433fb055899b
aisahabalive-ux <aisahabalive@gmail.com>
2026-02-15 17:28:13 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/supreme-v1000-hybrid/', // Aapki repo ka naam yahan hona chahiye
+  base: '/supreme-v1000-hybrid/', 
   build: {
     outDir: 'dist',
     emptyOutDir: true,
