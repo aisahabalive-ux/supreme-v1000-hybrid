81fb7d4304f49cc706bed736272bc7627ba31674
aisahabalive-ux <aisahabalive@gmail.com>
2026-02-15 15:21:11 +0530

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+if (typeof (window as any).process === 'undefined') {
+  (window as any).process = { env: {} };
+}
 
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(<React.StrictMode><App /></React.StrictMode>);
+}
