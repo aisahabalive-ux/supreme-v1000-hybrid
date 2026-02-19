986a63a66f1ca6932bf41891071bcf75a3c3ac20
aisahabalive-ux <aisahabalive@gmail.com>
2026-02-19 11:21:18 +0530

    Update app.tsx

--- app.tsx
+++ app.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useState } from 'react';
+import InfiniteGateway from './components/InfiniteGateway';
+import SovereignCore from './components/SovereignCore';
+import FileManager from './components/FileManager';
+import UniversalBanking from './components/UniversalBanking';
+import SupremeSecurity from './components/SupremeSecurity';
 
-// Simple Router setup using the same sections as index.html
 export default function App() {
-  return (
-    <Router>
-      <div className="bg-[#050505] text-white">
-        {/* Navigation logic yahan index.html wali hi rahegi */}
-        <Routes>
-          <Route path="/" element={<HomeContent />} />
-          {/* React Router will handle these paths if user navigates via JS */}
-        </Routes>
-      </div>
-    </Router>
-  );
-}
-
-function HomeContent() {
-  return (
-    <div>
-       {/* React automatically hooks into the #root div */}
-       {/* User ko index.html wala content dikhta rahega jab tak React hydrate nahi hota */}
-    </div>
-  );
+  const [view, setView] = useState('GATEWAY');
+  const render = () => {
+    switch(view) {
+      case 'GATEWAY': return <InfiniteGateway />;
+      case 'CORE': return <SovereignCore />;
+      case 'FILES': return <FileManager />;
+      case 'BANKING': return <UniversalBanking />;
+      case 'SECURITY': return <SupremeSecurity />;
+      default: return <InfiniteGateway />;
+    }
+  };
+  return <div className="bg-black min-h-screen text-white">{render()}</div>;
 }
